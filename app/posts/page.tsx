e1a57139d9730db399cb6f512a2997968769f099
JOHN-AMAO <amaojohn767@gmail.com>
2024-03-29 21:59:11 +0100

    .

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,15 +1,25 @@
 "use client";
-import { signOut } from "next-auth/react";
+import { useSession, signOut } from "next-auth/react";
+import Image from "next/image";
 import React from "react";
 
-const page = () => {
+const Page = () => {
+  const session = useSession();
+  console.log(session);
   return (
     <>
       {" "}
       <div>Hello users</div>
+      <h1>{session?.data?.user?.email}</h1>
+      <Image
+        src={session?.data?.user?.image}
+        width={50}
+        height={50}
+        alt='me'
+      />
       <button onClick={() => signOut()}>Log out</button>
     </>
   );
 };
 
-export default page;
+export default Page;
