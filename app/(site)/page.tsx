c8ea86d355a5e317164d0d733db15080baa474be
JOHN-AMAO <amaojohn767@gmail.com>
2024-03-29 20:52:02 +0100

    homepage created

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,9 +1,15 @@
-import { AuthForm } from "@/components/component/Authform";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <AuthForm />
+      <div className='bg-black text-white w-full h-full'>
+        <h1>This is the homepage</h1>
+        <Link href='/auth'>
+          <Button variant='outline'>Login</Button>
+        </Link>
+      </div>
     </>
   );
 }
