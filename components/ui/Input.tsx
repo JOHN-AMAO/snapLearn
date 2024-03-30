e5a121c3ad5e51fa6b82f9eaaa4e584d858f9e72
JOHN-AMAO <amaojohn767@gmail.com>
2024-03-30 22:05:13 +0100

    home page structure done

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -1,62 +1,25 @@
-import React from "react";
-import { UseFormRegister, FieldValues } from "react-hook-form";
+import * as React from "react"
 
-interface InputProps {
-  label: string;
-  id: string;
-  type?: string;
-  register: UseFormRegister<FieldValues>;
-}
+import { cn } from "@/lib/utils"
 
-const Input: React.FC<InputProps> = ({ label, id, type, register }) => {
-  return (
-    <>
-      <div className='relative'>
-        <input
-          type={type}
-          id={id}
-          {...register(id)}
-          className='
-          block
-          rounded-md
-          px-6
-          pt-6
-          pb-1
-          w-full
-          text-md
-          text-white
-          bg-gray-500
-          appearance-none
-          focus:outline-none
-          focus:ring-0 
-          peer'
-          placeholder=' '
-        />
-        <label
-          className='
-        absolute
-        text-md
-        text-zinc-400
-        duration-150
-        transform
-        -translate-y-3
-        scale-75
-        z-10
-        top-4
-        left-6
-        origin-[0]
-        peer-placeholder-shown:scale-100
-        peer-placeholder-shown-translate-y-0
-        peer-focus:scale-75
-        peer-focus:-translate-y-3
-        '
-          htmlFor='email'
-        >
-          {label}
-        </label>
-      </div>
-    </>
-  );
-};
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
 
-export default Input;
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={cn(
+          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    )
+  }
+)
+Input.displayName = "Input"
+
+export { Input }
